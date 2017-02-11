2fbc349fd2e67aeb06d3eecad4278c8269fa8908
Nah <nahung89@gmail.com>
2017-02-11 13:59:35 +0700

    init project

---
+++ InstagramCaption/InstagramCaption/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstagramCaption
+//
+//  Created by NAH on 2/11/17.
+//  Copyright © 2017 NAH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
